96a4f7beef8da84a8498e86beed486b95c7cd9c4
Prodev2017 <chris.bergman320@gmail.com>
2017-12-09 00:20:43 +0800

    add alert delete

--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -25,7 +25,8 @@ export class AlertsPage {
   alertlist:Array<any>;
   alert_opportunity:String;
   alertview:any;
-
+  itemdelete:any;
+  categorylist:any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -38,6 +39,12 @@ export class AlertsPage {
 			{alertname: 'name 2', history:'2', startprice:'40',lastprice:'50', distance: '20', startduration: 'July 6', lastduration:'July 14'},
 			{alertname: 'name 3', history:'24',startprice:'24',lastprice:'32', distance: '30', startduration: 'July 14', lastduration:'July 23'},
   	]
+    this.categorylist =
+    [
+      {alertname: 'John', history:'24', startprice:'20',lastprice:'30', distance: '10', startduration: 'July 1', lastduration:'July 10'},
+      {alertname: 'name 2', history:'2', startprice:'40',lastprice:'50', distance: '20', startduration: 'July 6', lastduration:'July 14'},
+      {alertname: 'name 3', history:'24',startprice:'24',lastprice:'32', distance: '30', startduration: 'July 14', lastduration:'July 23'},
+    ]
     this.alert_opportunity="alert";
     this.itemprovider.Getalerthistory(localStorage.getItem('uid')).subscribe(data=>{
 
@@ -51,6 +58,7 @@ export class AlertsPage {
     });
     this.alertview=true;
     console.log("constructer");
+    this.itemdelete=false
   }
 
   ionViewDidLoad() {
@@ -70,6 +78,22 @@ export class AlertsPage {
       this.alertview=true;
     }
     else this.alertview=false;
+  }
+
+  deletealert(n){
+    for (var i = 0; i <this.list.length; i++) {
+      if(this.list[i]==n){
+        this.list.splice(i, 1);
+      }
+    }
+  }
+
+  deletecategory(n){
+    for (var i = 0; i <this.categorylist.length; i++) {
+      if(this.categorylist[i]==n){
+        this.categorylist.splice(i, 1);
+      }
+    }
   }
 
 }
